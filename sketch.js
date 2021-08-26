0a58e7b52197728958db2e2ce04bef3a6b84b8f8
mdude977 <72481769+mdude977@users.noreply.github.com>
2021-08-25 20:15:11 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var canva ,gameState, contestantCount, database,answer, quiz, question, allContestants;
+var contestant;
+var gameState = 0
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState()
+  quiz.start()
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount ===2){
+    quiz.update(1);
+  }
+if(gameState === 1){
+  clear();
+  quiz.play();
+}
+  
+}
